2bce9cba319618d98cd35e80603c9ad52335ba81
Henry Peteet <hvpeteet@gmail.com>
2018-01-20 15:22:00 -0500

    Added rust impl

---
+++ week1/rust/otp/target/debug/libotp.d
@@ -0,0 +1,1 @@
+C:\Users\hvpet\Documents\github\crypto1\week1\rust\otp\target\debug\libotp.rmeta: C:\Users\hvpet\Documents\github\crypto1\week1\rust\otp\src\main.rs
---
+++ week1/rust/otp/target/debug/otp.d
@@ -0,0 +1,1 @@
+C:\Users\hvpet\Documents\github\crypto1\week1\rust\otp\target\debug\otp.exe: C:\Users\hvpet\Documents\github\crypto1\week1\rust\otp\src\main.rs
